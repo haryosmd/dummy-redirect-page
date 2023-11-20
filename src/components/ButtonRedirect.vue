d7b7ff92d81a2ddc7ce07b870d6acefdd0d310d4
Slamet Haryo Samudro <haryosmd@gmail.com>
2023-11-21 02:53:41 +0700

    add new button env

--- src/components/ButtonRedirect.vue
+++ src/components/ButtonRedirect.vue
@@ -3,19 +3,58 @@
     <div class="wording-section">
       <h1 class="head-section">Login Prakerja</h1>
       <p>Ceritanya ini udah sukses verifikasi wajah.</p>
+      <div class="state-action-wrapper">
+        <button @click="handleChangeEnv" class="outline-primary-button">
+        {{ buttonWording }}
+        </button>
+        <p>URL: {{env}}</p>
+      </div>
     </div>
-    <button @click="redirectToCareer" class="outline-primary-button">
-      Redirect to Karier.mu
-    </button>
+    <div class="button-wrapper">
+      <button @click="redirectToKariermuSuccess" class="outline-primary-button">
+        Redirect to Prakerja Karier.mu (Success)
+      </button>
+      <button @click="redirectToKariermuErrorWrongFace" class="outline-error-button">
+        Redirect to Prakerja Karier.mu (Error Wrong Face)
+      </button>
+      <button @click="redirectToKariermuErrorLimit" class="outline-error-button">
+        Redirect to Prakerja Karier.mu (Error Limit 5 times)
+      </button>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      env: 'localhost:8080',
+      isLocal: true
+    }
+  },
+  computed: {
+    buttonWording() {
+      return this.isLocal ? 'Ubah Domain ke Kariermu' : 'Ubah Domain ke Local';
+    }
+  },
   methods: {
-    redirectToCareer() {
-      window.location.href = "https://www.karier.mu";
+    redirectToKariermuSuccess() {
+      window.location.href = `${this.env}/prakerja-face-recognition-status?state=OAUTHSTATECODE`;
     },
+    redirectToKariermuErrorWrongFace() {
+      window.location.href = `${this.env}/prakerja-face-recognition-status?error=terjadi%20kesalahan&error_code=ERRFR0001&error_description=wajah%20tidak%20sesuai&state=OAUTHSTATECODE`;
+    },
+    redirectToKariermuErrorLimit() {
+      window.location.href = `${this.env}/prakerja-face-recognition-status?error=terjadi%20kesalahan&error_code=ERRLV0001&error_description=Kamu%20sudah%20pernah%20redeem%20pelatihan%20ini%20sebelumnya&state=OAUTHSTATECODE`;
+    },
+    handleChangeEnv() {
+      if (this.isLocal) {
+        this.env = 'https://www.prakerja.dev.karier.mu';
+      } else {
+        this.env = 'localhost:8080';
+      }
+      this.isLocal = !this.isLocal;
+    }
   },
 };
 </script>
@@ -40,6 +79,21 @@ export default {
   color: #006AD3;
 }
 
+.state-action-wrapper {
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  gap: 16px;
+  margin-top: 8px;
+}
+
+.button-wrapper {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 14px;
+}
+
 .outline-primary-button {
   font-size: 16px;
   color: #006AD3;
@@ -57,8 +111,31 @@ export default {
   line-height: var(--UI-text-label-1-line-height);
 }
 
+.outline-error-button {
+  font-size: 16px;
+  color: #FF3838;
+  border: 1px solid #FF3838;
+  background-color: transparent;
+  cursor: pointer;
+  border-radius: 20px;
+  padding: 8px 16px;
+  transition: background-color 0.2s, color 0.2s;
+  font-family: var(--UI-text-label-1-font-family);
+  font-size: var(--UI-text-label-1-font-size);
+  font-style: var(--UI-text-label-1-font-style);
+  font-weight: var(--UI-text-label-1-font-weight);
+  letter-spacing: var(--UI-text-label-1-letter-spacing);
+  line-height: var(--UI-text-label-1-line-height);
+}
+
+
 .outline-primary-button:hover {
   border: 1px solid #006AD3;
+  background-color: #F5F8FA;
+}
+
+.outline-error-button:hover {
+  border: 1px solid #FF3838;
   background-color: #F5F8FA;
 }
 </style>
